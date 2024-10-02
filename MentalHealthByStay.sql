3c2e56c88c35d0c2269277a7450bbb5657e6649c
Dogukan <62773717+DogukanTorun1@users.noreply.github.com>
2024-10-02 20:57:23 +0300

    Update MentalHealthByStay.sql

--- MentalHealthByStay.sql
+++ MentalHealthByStay.sql
@@ -1,1 +1,6 @@
-
+Select stay, count(stay) as count_int, ROUND(avg(todep), 2) as average_phq, ROUND(avg(tosc), 2) as average_scs, ROUND(avg(toas), 2) as average_as
+From students
+Where inter_dom = ''
+Group by stay
+Order by stay DESC
+Limit 9
